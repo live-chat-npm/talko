eb2f921b88cded0b69ba00f940ca5e0cf2eeb832
Jonathon Flores <jonathon@Jonathons-MacBook-Pro.local>
2019-02-11 09:27:15 -0600

    created modal for chat component

---
+++ src/Chat.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import { ThemeProvider } from "styled-components";
+import logo from "./images/logo.jpg";
+import sendButton from "./images/send-button.png";
+import {
+  ChatWindow,
+  Input,
+  SendButton,
+  MinimizedChatWindow,
+  Header,
+  Profile,
+  ProfileImage,
+  Name,
+  Title,
+  Logo,
+  MessageWindow,
+  Message,
+  MaximizeButton,
+  MinimizeButton,
+  Footer
+} from "./ChatComponents";
+
+export default class Chat extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages: ["Hello, how may we help you?"],
+      input: "",
+      minimized: true //Default position for chat window
+    };
+  }
+
+  //reference to the newest message in the message window
+  newMessage = React.createRef();
+
+  componentDidUpdate() {
+    if (this.newMessage.current) {
+      this.scrollToBottom();
+    }
+  }
+
+  //Keeps the message window scrolled to the newest message
+  scrollToBottom = () => {
+    this.newMessage.current.scrollIntoView();
+  };
+
+  //Minimizes or maximizes the chat window
+  toggleChatWindow = () => {
+    this.setState({
+      minimized: !this.state.minimized
+    });
+  };
+
+  //User input to send messages
+  handleInput = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  //Adds the message to the messages array in state
+  sendMessage = () => {
+    this.setState({
+      messages: [...this.state.messages, this.state.input],
+      input: ""
+    });
+  };
+
+  render() {
+    //Theme object holds css values that are passed into the theme provider
+    const theme = {
+      background: "",
+      color: "",
+      input: "",
+      header: ""
+    };
+
+    //Switch determines what values are held by the theme object based on the selected theme
+    switch (this.props.theme) {
+      case "light":
+        theme.background = "#fff";
+        theme.color = "#0d0d0d";
+        theme.input = "lightgray";
+        theme.header = "#efefef";
+        break;
+      case "dark":
+        theme.background = "#0d0d0d";
+        theme.color = "#0d0d0d";
+        theme.input = "lightgray";
+        theme.header = "000";
+        break;
+      default:
+        console.log("default theme");
+    }
+
+    let dispMessages = this.state.messages.map((message, index) => {
+      return (
+        <Message key={index}>
+          <p style={{ margin: "1px", fontSize: "10px", fontWeight: "lighter" }}>
+            {this.props.name} {new Date().toLocaleTimeString()}
+          </p>
+          {message}
+        </Message>
+      );
+    });
+
+    return (
+      <ThemeProvider theme={theme}>
+        {this.state.minimized ? (
+          <MinimizedChatWindow>
+            <div
+              style={{
+                fontSize: "10px",
+                paddingLeft: "10px",
+                color: "#575757"
+              }}
+            >
+              <h1>Live Chat</h1>
+            </div>
+            <MaximizeButton onClick={this.toggleChatWindow}>
+              &and;
+            </MaximizeButton>
+          </MinimizedChatWindow>
+        ) : (
+          <ChatWindow>
+            <Header>
+              <div style={{ fontSize: "10px", paddingLeft: "10px" }}>
+                <h1>Live Chat</h1>
+              </div>
+              <MinimizeButton onClick={this.toggleChatWindow}>
+                &or;
+              </MinimizeButton>
+            </Header>
+            <Profile>
+              <ProfileImage src={this.props.profileImage} />
+              <div>
+                <Name>{this.props.name}</Name>
+                <Title>{this.props.title}</Title>
+              </div>
+              <Logo src={logo} />
+            </Profile>
+            <MessageWindow>
+              {dispMessages} <div ref={this.newMessage} />
+            </MessageWindow>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                height: "50px",
+                borderTop: "solid 2px lightgray",
+                borderBottom: "solid 2px lightgray"
+              }}
+            >
+              <Input
+                onChange={this.handleInput}
+                value={this.state.input}
+                placeholder="Type in your message here..."
+              />
+              <SendButton src={sendButton} onClick={this.sendMessage} />
+            </div>
+            <Footer>
+              <p
+                style={{
+                  margin: 0,
+                  marginRight: "10px",
+                  textAlign: "right"
+                }}
+              >
+                Powered by Talko.io
+              </p>
+            </Footer>
+          </ChatWindow>
+        )}
+      </ThemeProvider>
+    );
+  }
+}
---
+++ src/ChatComponents.js
@@ -0,0 +1,179 @@
+import styled, { keyframes } from "styled-components";
+
+const Maximize = keyframes`
+  from {
+    height: 10vh
+    width: 300px;
+  } 
+
+  to {
+    height: 50vh
+    width: 368px;
+  }
+`;
+
+const Minimize = keyframes`
+      from {
+        height: 50vh
+        width: 368px;
+      } 
+
+      to {
+        height: 10vh
+        width: 300px;
+      }
+     `;
+
+const FadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: .1;
+  }
+ `;
+
+//Parent component for entire chat window
+export const ChatWindow = styled.div`
+  background: ${props => props.theme.background};
+  border-radius: 3px;
+  color: #575757;
+  height: 61vh;
+  width: 368px;
+  animation: ${Maximize} 0.2s linear;
+  box-shadow: 0px 0px 10px 1px lightgray;
+`;
+
+//Input field for user input
+export const Input = styled.input`
+  width: 350px;
+  height: 35px;
+  background: transparent;
+  font-size: 20px;
+  color: ${props => props.theme.color};
+  border: none;
+  outline: none;
+
+  &::-webkit-input-placeholder {
+    padding-left: 2px;
+    opacity: 0.3;
+  }
+
+  &:-moz-placeholder {
+    padding-left: 2px;
+    opacity: 0.3;
+  }
+
+  &::-moz-placeholder {
+    padding-left: 2px;
+    opacity: 0.3;
+  }
+
+  &:-ms-input-placeholder {
+    padding-left: 2px;
+    opacity: 0.3;
+  }
+`;
+
+export const SendButton = styled.img`
+  opacity: 0.1;
+  padding-right: 15px;
+  cursor: pointer;
+  margin-bottom: 0;
+  animation: ${FadeIn} 0.3s linear;
+`;
+
+export const MinimizedChatWindow = styled.div`
+  display: flex;
+  align-items: center;
+  background: #efefef;
+  border-radius: 3px;
+  height: 5vh;
+  width: 300px;
+  animation: ${Minimize} 0.2s linear;
+  box-shadow: 0px 0px 10px 1px lightgray;
+`;
+
+export const Header = styled.div`
+  height: 5vh;
+  width: 100%;
+  background: ${props => props.theme.header};
+  display: flex;
+  align-items: center;
+`;
+
+//Parent div for profile image, name, title, and logo
+export const Profile = styled.div`
+  display: flex;
+  height: 11vh;
+  width: 100%;
+  background: #fff;
+  box-shadow: 0px 0px 10px 1px #eeecec;
+`;
+
+export const ProfileImage = styled.img`
+  height: 8vh;
+  object-fit: contain;
+  margin: 10px;
+`;
+
+export const Name = styled.h1`
+  font-size: 20px;
+  color: #575757;
+  margin-bottom: 2px;
+`;
+
+export const Title = styled.h1`
+  font-size: 15px;
+  color: #575757;
+  margin: 0;
+  font-weight: lighter;
+`;
+
+export const Logo = styled.img`
+  height: 8vh;
+  width: 8vh;
+  object-fit: contain;
+  margin: 10px;
+  margin-left: auto;
+  margin-right: 15px;
+  box-shadow: 0px 0px 10px 1px lightgray;
+`;
+
+export const MessageWindow = styled.div`
+  height: 35vh;
+  overflow: scroll;
+`;
+
+export const MaximizeButton = styled.div`
+  position: absolute;
+  right: 15px;
+  cursor: pointer;
+  font-weight: bolder;
+  color: #575757;
+  transform: scaleX(2);
+`;
+
+export const MinimizeButton = styled.div`
+  position: absolute;
+  right: 15px;
+  cursor: pointer;
+  font-weight: bolder;
+  color: #575757;
+  transform: scaleX(2);
+`;
+
+export const Footer = styled.div`
+  width: 100%;
+  font-size: 10px;
+  background-color: #efefef;
+  height: 18px;
+`;
+
+export const Message = styled.div`
+  font-size: 14px;
+  color: #575757;
+  padding-left: 10px;
+  padding-top: 10px;
+`;
